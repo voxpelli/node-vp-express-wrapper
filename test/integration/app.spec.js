8cd4ca539803d9d5a534c8d6bfd11487edff6c5d
Pelle Wessman <pelle@kodfabrik.se>
2015-11-27 13:23:03 +0100

    tests(wrapper): add tests for the latest additions

--- test/integration/app.spec.js
+++ test/integration/app.spec.js
@@ -1,12 +1,36 @@
 'use strict';
 
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
 const request = require('supertest-as-promised');
+
+chai.use(chaiAsPromised);
+chai.should();
 
 describe('Express Wrapper App', function () {
   const ExpressWrapper = require('../../');
 
+  let config;
+
+  beforeEach(function () {
+    config = ExpressWrapper.ExpressConfig.getDefaultConfig({});
+  });
+
   describe('basic', function () {
     it('should return a web page on the "/" path', function () {
+      const wrapperInstance = new ExpressWrapper(config);
+      const app = wrapperInstance.getApp();
+
+      return request(app)
+        .get('/')
+        .expect(200)
+        .expect('Content-Type', 'text/html; charset=utf-8')
+        .should.eventually.have.property('text')
+          .that.is.a('string')
+          .that.equals('<h1>Hello World!</h1>');
+    });
+
+    it('should allow overriding the "/" path', function () {
       const protoProps = {};
 
       protoProps.getRoutes = function () {
@@ -19,15 +43,33 @@ describe('Express Wrapper App', function () {
 
       const WrapperClass = ExpressWrapper.extend(protoProps);
 
-      const config = ExpressWrapper.ExpressConfig.getDefaultConfig({});
-
       const wrapperInstance = new WrapperClass(config);
       const app = wrapperInstance.getApp();
 
       return request(app)
         .get('/')
         .expect(200)
-        .expect('Content-Type', 'text/html; charset=utf-8');
+        .expect('Content-Type', 'text/html; charset=utf-8')
+        .should.eventually.have.property('text')
+          .that.is.a('string')
+          .that.equals('<p>some html</p>');
+    });
+
+    it('should allow removing the "/" path', function () {
+      const protoProps = {};
+
+      protoProps.getRoutes = function () {
+        return {};
+      };
+
+      const WrapperClass = ExpressWrapper.extend(protoProps);
+
+      const wrapperInstance = new WrapperClass(config);
+      const app = wrapperInstance.getApp();
+
+      return request(app)
+        .get('/')
+        .expect(404);
     });
   });
 });
